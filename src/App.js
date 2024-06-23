08011b686fdc78ad7aea05eff2fcc3681b555711
DarawanOmar <darait721@gmail.com>
2024-06-23 16:51:58 +0300

    Check Last Updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
-import { useSelector  } from 'react-redux'
+import { useSelector } from "react-redux";
 import Routess from "./components/Routes";
 import { AnimelContextProvider } from "./context/AnimelContext";
 
-
+//  Test Last Updated
 function App() {
-
   // const dispatch = useDispatch()
   // const {foods} = useSelector((state)=> state.count)
 
-  // useEffect(()=>{ 
-  //     dispatch(fetchData())   
+  // useEffect(()=>{
+  //     dispatch(fetchData())
   // },[])
 
   // useEffect(()=>{
   //   dispatch(totalCalculator())
   // },[foods])
-  const darkValue = useSelector((state)=>state.dark.isDark);
+  const darkValue = useSelector((state) => state.dark.isDark);
 
   return (
     <div
-    className={darkValue ? 'bg-black text-white duration-700 ' : 'bg-white text-black duration-700'}>  
-    <AnimelContextProvider>
+      className={
+        darkValue
+          ? "bg-black text-white duration-700 "
+          : "bg-white text-black duration-700"
+      }
+    >
+      <AnimelContextProvider>
         <BrowserRouter>
-          <Navbar/>
-          <Routess/>
-        </BrowserRouter>     
-      </AnimelContextProvider>  
+          <Navbar />
+          <Routess />
+        </BrowserRouter>
+      </AnimelContextProvider>
     </div>
   );
 }
